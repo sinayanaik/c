6874b1f3014dea8457a840c6b8d12b9fb5f9e855
sinaya <sinayanaik@gmail.com>
2021-04-17 03:55:38 +0100

    operators

---
+++ 3.operators/test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("a: ");
+    scanf("%d",&a);
+    
+    printf("b: ");
+    scanf("%d",&b);
+
+    printf("a = %d\n",a);
+    printf("b = %10d\n",b);
+
+
+}
